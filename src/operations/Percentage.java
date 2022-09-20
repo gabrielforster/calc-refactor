66ed096a50506959b49d678477ccc696879eddda
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-20 20:22:24 -0300

    fix: fixing percentaga class

--- src/operations/Percentage.java
+++ src/operations/Percentage.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 public class Percentage {
     private double total;
     private double amount;
-
+    private double result;
 
     public Percentage(double total, double amount){
         this.total = total;
         this.amount = amount;
+        this.setResult((this.amount * 100) / this.total);
     }
 
-    private double result = (amount * 100) / total;
+    public String getResult() {
+        return String.format("%.2f", result);
+    }
 
-    public double getResult() {
-        return result;
+    public void setResult(double result) {
+        this.result = result;
     }
 }
