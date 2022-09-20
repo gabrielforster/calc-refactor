1a06aef36ec362ca578f2c382bc3d94ecc62537c
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-20 20:23:50 -0300

    fix: fixing potentiation class

--- src/operations/Potentiation.java
+++ src/operations/Potentiation.java
@@ -4,16 +4,19 @@ public class Potentiation {
 
     private double base;
     private double potency;
+    private double result;
 
     public Potentiation(double base, double potency) {
         this.base = base;
         this.potency = potency;
+        setResult(Math.pow(this.base, this.potency));
     }
-
-    private double result = Math.pow(base, potency);
 
     public double getResult() {
         return result;
     }
 
+    public void setResult(double result) {
+        this.result = result;
+    }
 }
