079db510838dac5d9765a2d43a321077d1d31014
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-20 20:44:18 -0300

    feat: making the switch case for the operations

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import operations.*;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,13 +25,13 @@ public class Main {
 
             switch (option) {
                 case(1): {
-                    Soma calc = new Soma();
+                    Sum calc = new Sum();
                     System.out.println("Quantos números você deseja somar?");
                     quantityOfNumbers = scanner.nextInt();
                     ArrayList<Integer> numList = new ArrayList();
 
                     for (int i = 0; quantityOfNumbers > i; i++){
-                        System.out.println("Digite um número para somar");
+                        System.out.println("Digite um número a ser somado:");
                         numList.add(scanner.nextInt());
                     }
 
@@ -39,7 +41,81 @@ public class Main {
                     break;
                 }
                 case(2): {
+                    Subtraction calc = new Subtraction();
+                    System.out.println("Quantos números você deseja subtrair?");
+                    quantityOfNumbers = scanner.nextInt();
+                    ArrayList<Integer> numList = new ArrayList();
 
+                    for (int i = 0; quantityOfNumbers > i; i++){
+                        System.out.println("Digite um número a ser subtraido:");
+                        numList.add(scanner.nextInt());
+                    }
+
+                    calc.doTheSubtraction(numList);
+
+                    System.out.println("Resultado: " + calc.getResult());
+                    break;
+                }
+                case(3):{
+                    System.out.println("Primeiro digite o valor a ser dividido e depois por qual quer dividir:" +
+                            "Número a ser dividido: ");
+                    double divider = scanner.nextDouble();
+                    System.out.println(divider + " dividido por: ");
+                    double dividend = scanner.nextDouble();
+
+                    Division calc = new Division(divider, dividend);
+
+                    System.out.println("Resultado: " + calc.getResult());
+                    break;
+                }
+                case(4):{
+                    Multiplication calc = new Multiplication();
+                    System.out.println("Quantos números você deseja multiplicar?");
+                    quantityOfNumbers = scanner.nextInt();
+                    ArrayList<Integer> numList = new ArrayList();
+
+                    for (int i = 0; quantityOfNumbers > i; i++){
+                        System.out.println("Digite um número a ser multiplicado:");
+                        numList.add(scanner.nextInt());
+                    }
+
+                    calc.doTheMultiplication(numList);
+
+                    System.out.println("Resultado: " + calc.getResult());
+                    break;
+                }
+                case(5):{
+                    System.out.println("Primeiro digite o valor a ser elevado, e depois a potência:" +
+                            "Número a ser elevado: ");
+                    double base = scanner.nextDouble();
+                    System.out.println(base + " elevado á: ");
+                    double potency = scanner.nextDouble();
+
+                    Potentiation calc = new Potentiation(base, potency);
+
+                    System.out.println("Resultado: " + calc.getResult());
+                    break;
+                }
+                case(6): {
+                    SquareRoot calc = new SquareRoot();
+                    System.out.println("Qual número você deseja descobrir a raiz quadrada?");
+
+                    calc.doTheSquareRoot(scanner.nextDouble());
+
+                    System.out.println("Resultado: " + calc.getResult());
+                    break;
+                }
+                case(7): {
+                    System.out.println("Digite o total e depois a sua quantidade." +
+                            "Total: ");
+                    double total = scanner.nextDouble();
+                    System.out.println("Sua quantidade: ");
+                    double amount = scanner.nextDouble();
+
+                    Percentage calc = new Percentage(total, amount);
+
+                    System.out.println("Resultado: " + calc.getResult() + "%");
+                    break;
                 }
                 case(8):{
                     System.out.println("Saindo da calculadora");
